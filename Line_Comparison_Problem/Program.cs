36a79b0a49a2913be49725d404b573bd83f7f700
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-06-30 07:13:23 +0530

    Fourth Commit

--- Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Program.cs
@@ -28,16 +28,18 @@ namespace Line_Comparison_Problem
 
                 //'0' for checking equality of 2 lengths
                 //'1' for comparing 2 lengths
-
+                //'2' for exit
                 int option = int.Parse(Console.ReadLine());
                 switch (option)
                 {
                     case 0:
                         Program.checkEquality(lengthArray[1].ToString(), lengthArray[2].ToString());
                         break;
-                    //case 1:
-                    //Program.comparingTheLines(lengthArray[1].ToString(), lengthArray[2].ToString());
-                    //break;
+                    case 1:
+                    Program.comparingTheLines(lengthArray[1].ToString(), lengthArray[2].ToString());
+                    break;
+                    case 2:
+                        return;
 
                     default:
                         break;
@@ -51,6 +53,7 @@ namespace Line_Comparison_Problem
                 double lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
                 return lineLength;
         }
+        //Function to find equality of 2 lengths
         public static void checkEquality(string length1, string length2)
         {
             if (length1.Equals(length2))
@@ -61,6 +64,25 @@ namespace Line_Comparison_Problem
             {
                 Console.WriteLine("The Lines are not at equal length: L1: " + length1 + " L2: " + length2);
             }
+        }
+
+        //Function to compare the length of 2 lines
+
+        public static void comparingTheLines(string length1, string length2)
+        {
+            if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("The Line1 with length " + length1 + " is greater than Line2 with length " + length2);
+            }
+            else if (length1.CompareTo(length2) < 0)
+            {
+                Console.WriteLine("The Line2 with length " + length2 + " is greater than Line1 with length " + length1);
+            }
+            else
+            {
+                Console.WriteLine("Both Lines are of same length");
+            }
+
         }
 
     }
